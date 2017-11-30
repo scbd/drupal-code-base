403964af98ca9ac9cac2a8a77273c5ce9308bf56
Randy Houlahan <randy.houlahan@cbd.int>
2017-11-30 08:23:13 -0500

    ad security check on php packages.  Will display and security issues on container int or on requiring a new composer package

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+php security-checker.phar security:check ../composer.lock
 echo "Composer update"
-composer update
+composer update -d=$APP_ROOT
 # echo "change path to web"
 # cd web
 echo "Drush config import"
 drush -r -y "/var/www/html/web" cim
 echo "Drush cache clear and rebuild"
-drush -r -y "/var/www/html/web" cr
+drush -r -y "/var/www/html/web" cr drush
