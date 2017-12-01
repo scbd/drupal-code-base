1370d2363d8da4412ff4ab8a9b9b4ee9bcc55c48
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-01 08:18:16 -0500

    path corrections

--- sh/actions/bootstrap.sh
+++ sh/actions/bootstrap.sh
@@ -4,8 +4,7 @@ echo "Composer init"
 
 composer status -d=$APP_ROOT
 
-cd /var/www/html/web
 echo "Drush config import"
-drush -r -y cim
+drush -r $APP_ROOT/web -y cim
 echo "Drush cache clear and rebuild"
-drush -r -y cr drush
+drush -r $APP_ROOT/web -y cr drush
--- sh/actions/init.sh
+++ sh/actions/init.sh
@@ -15,9 +15,9 @@ fi
 echo "Wait for db"
 if [ -f "/run/secrets/settings.php" ]
 then
-	$APP_ROOT/wait-for-it.sh drupal-1.cluster-c1c5sybjelgu.us-east-1.rds.amazonaws.com:3306 --timeout=0 --strict -- sh $APP_ROOT/bootstrap.sh
+	$APP_ROOT/sh/actions/wait-for-it.sh drupal-1.cluster-c1c5sybjelgu.us-east-1.rds.amazonaws.com:3306 --timeout=0 --strict -- sh $APP_ROOT/sh/actions/bootstrap.sh
 else
-	$APP_ROOT/wait-for-it.sh DRUPAL_mariadb:3306 --timeout=0 --strict -- sh $APP_ROOT/bootstrap.sh
+	$APP_ROOT/sh/actions/wait-for-it.sh DRUPAL_mariadb:3306 --timeout=0 --strict -- sh $APP_ROOT/sh/actions/bootstrap.sh
 fi
 
 
