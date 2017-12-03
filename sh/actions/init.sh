712fac6ad1708917443c9f5e4d4a9c29da5ddf52
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-03 11:02:19 -0500

    do not remove

--- sh/actions/init.sh
+++ sh/actions/init.sh
@@ -7,10 +7,10 @@ fi
 
 if [ -f "config.tgz.enc" ]
 then
-    echo "Decrypt config -- $APP_ROOT"
+    echo "Decrypt config -- "$APP_ROOT
     openssl enc -d -aes-256-cbc -in config.tgz.enc -k `cat /run/secrets/SRCKEY` | tar xz
-    rm config.tgz.enc
-    rm config.tgz
+    # rm config.tgz.enc
+    # rm config.tgz
     ls -al config
 fi
 
