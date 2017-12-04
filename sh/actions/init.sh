2fec7b0588f2d70d6b4e3160bc01c1611fa7463c
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-04 10:55:35 -0500

    no delete

--- sh/actions/init.sh
+++ sh/actions/init.sh
@@ -11,9 +11,9 @@ then
     then
       echo "Decrypt config -- "$APP_ROOT
       openssl enc -d -aes-256-cbc -in config.tgz.enc -k `cat /run/secrets/SRCKEY` | tar xz
-      
+      #rm config.tgz.enc
       tar -xvzf config.tgz
-      rm config.tgz
+      #rm config.tgz
       mv config/config /var/www/files/config/sync_dir
       rm -f config
     fi
