f29981231c9e0b67a40b7ee9db3e5faf58cf2655
Randy Houlahan <randy.houlahan@cbd.int>
2017-12-06 07:42:32 -0500

    remove su

--- sh/actions/init.sh
+++ sh/actions/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-su www-data
+
 if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
@@ -10,7 +10,7 @@ then
     if [ -f "/run/secrets/settings.php" ]
     then
       echo "Decrypt config -- "$APP_ROOT
-      sudo -u www-data openssl enc -d -aes-256-cbc -in config.tgz.enc -k `cat /run/secrets/SRCKEY` | tar xz
+      openssl enc -d -aes-256-cbc -in config.tgz.enc -k `cat /run/secrets/SRCKEY` | tar xz
       #rm config.tgz.enc
       mv config/config/* /var/www/files/config/sync_dir
       rm -rf config
